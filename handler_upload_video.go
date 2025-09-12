428b0854c893233aec3d38bc5ca4df5ef3cea213
Craig Bucher <craigbucher@hotmail.com>
2025-09-12 19:04:16 -0400

    aspect ratio sort

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 	"mime"
 	"net/http"
 	"os"
+	"os/exec"
+	"path"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -96,8 +102,28 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
+	// initialize empty 'directory' string:
+	directory := ""
+	// Call getVideoAspectRatio (below) to get aspect ratio of video:
+	aspectRatio, err := getVideoAspectRatio(tempFile.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error determining aspect ratio", err)
+		return
+	}
+	switch aspectRatio {
+	case "16:9":
+		directory = "landscape"
+	case "9:16":
+		directory = "portrait"
+	default:
+		directory = "other"
+	}
+
 	// Generate random 32-bit hex filename with extension:
 	key := getAssetPath(mediaType)
+	// Join directory and key = directory/filename:
+	key = path.Join(directory, key)
+
 	// Put the object into S3 using PutObject. You'll need to provide:
 	//	* The bucket name
 	//	* The file key. Use the same <random-32-byte-hex>.ext format as the key
@@ -128,3 +154,59 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	respondWithJSON(w, http.StatusOK, video)
 }
 
+func getVideoAspectRatio(filePath string) (string, error) {
+	// use exec.Command to run the same ffprobe command. In this case, the command is ffprobe 
+	// and the arguments are -v: error, -print_format: json, -show_streams, and the file path:
+	cmd := exec.Command("ffprobe",
+		"-v", "error",
+		"-print_format", "json",
+		"-show_streams",
+		filePath,
+	)
+
+	//  allocate an in-memory growable buffer:
+	var stdout bytes.Buffer
+	// redirect the command's stdout to that buffer:
+	cmd.Stdout = &stdout
+
+	// runs the command and handle errors inline:
+	if err := cmd.Run(); err != nil {
+		return "", fmt.Errorf("ffprobe error: %v", err)
+	}
+
+	// create an anonymous struct type used as a JSON target for unmarshaling ffprobe's output:
+	var output struct {
+		// The JSON has a top-level key 'streams' mapping to an array:
+		Streams []struct {
+			Width  int `json:"width"`
+			Height int `json:"height"`
+		} `json:"streams"`
+	}
+	// take the JSON bytes from the stdout buffer (the output from the ffprobe command) and parse 
+	// it into the output variable:
+	// (&output uses the address-of operator because json.Unmarshal needs a pointer to the struct 
+	// so it can modify it directly with the parsed JSON data)
+	if err := json.Unmarshal(stdout.Bytes(), &output); err != nil {
+		return "", fmt.Errorf("could not parse ffprobe output: %v", err)
+	}
+	// get the length (number of elements) in the Streams slice:
+	// if the slice is empty (has zero elements), return an error:
+	// (When ffprobe analyzes a video file, it returns information about all the streams in the file 
+	// (video streams, audio streams, subtitle streams, etc.). For this function to work, we need at 
+	// least one video stream to get the width and height dimensions)
+	if len(output.Streams) == 0 {
+		return "", errors.New("no video streams found")
+	}
+	// access the Width and Height fields of the first stream. These fields contain the pixel 
+	// dimensions of the video:
+	width := output.Streams[0].Width
+	height := output.Streams[0].Height
+
+	// return a text string of the aspect ratio:
+	if width == 16*height/9 {
+		return "16:9", nil
+	} else if height == 16*width/9 {
+		return "9:16", nil
+	}
+	return "other", nil
+}
